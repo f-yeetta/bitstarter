72db7ef9b9323ad98b8bd4550f681c0f503f5aef
Florus Y Both <f@yeetta.net>
2013-07-04 08:56:18 +0000

    added var js to web.js

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
+
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buf = new Buffer(fs.readFileSync("index.html")); 
-  response.send(buf.toString();
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
